70bb287f0cd12acae719d934049e6a4dce54f9a9
Julian Schutsch <julian.schutsch@sluonics.com>
2016-05-02 22:21:14 +0200

    remove obsolete comments

--- scripts/c_operations.py
+++ scripts/c_operations.py
@@ -1,10 +1,3 @@
-# Operations file for clifford algebra
-# Supposed to describe set of operations...
-# Must be limited to the operations provided by the environment
-# Lets define: operand1..n
-# Issue: Limiting the operand types...
-# Defining blades, it must be possible to add blades...
-
 op1, op2 = operands(blade1, blade1)
 
 print(op1)
